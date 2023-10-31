988e0d28e53101639df00c97ceaf1c191df27007
andreynikonov23 <xxxkeep3rxxx@gmail.com>
2023-10-31 14:14:39 +0300

    31.10

--- solution1/c/file.c
+++ solution1/c/file.c
@@ -1,6 +1,15 @@
 #include <stdio.h>
 
-int main(){
+int main () {
+    char buffer[256];
+    FILE *file1 = fopen("test1.txt", "r");
+    FILE *file2 = fopen("test2.txt", "w");
+    while ((fgets(buffer, 256, file1)) != NULL)
+    {
+        fputs(buffer, file2);
+    }
+    fclose(file1);
+    fclose(file2);
 
     return 0;
 }
