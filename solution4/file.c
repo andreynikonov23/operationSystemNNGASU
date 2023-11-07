ffb65df21fb71c70043753b3ce426c06c87f32d6
andrey <xxxkeep3rxxx@gmail.com>
2023-11-07 16:31:14 +0300

    07.11 2

--- solution4/file.c
+++ solution4/file.c
@@ -19,13 +19,25 @@ int main(int argc, char const *argv[]){
         int index = 1;
         char* o = opt[index];
         if (o == 'a'){
-            aFunc(opt[index + 1], opt, index + 1);
+            if (isMassiveFinished(opt, index+1) != 1){
+                aFunc(opt[index + 1], opt, index + 1);
+            } else {
+                printf("a\n");
+            }
         }
         if (o == 'b'){
-            bFunc(opt, index + 1);
+            if (isMassiveFinished(opt, index+1) != 1){
+                bFunc(opt, index + 1);
+            } else {
+                printf("b\n");
+            }
         }
         if (o == 'c'){
-            cFunc(opt[index + 1], opt, index + 1);
+            if (isMassiveFinished(opt, index+1) != 1){
+                cFunc(opt[index + 1], opt, index + 1);
+            } else {
+                printf("c\n");
+            }
         }
     }
 
@@ -33,33 +45,65 @@ int main(int argc, char const *argv[]){
 }
 
 int aFunc(char* neighbour, char* opt[], int* index){
+    printf("a\n");
     int i = index + 1;
     free(index);
     if (neighbour == 'a'){
-        aFunc(opt[i], opt, i);
+        if (isMassiveFinished(opt, i) != 1){
+            aFunc(opt[i], opt, i);
+        }
     }
     if (neighbour == 'b'){
-        bFunc(opt, i);
+        if (isMassiveFinished(opt, i) != 1){
+            bFunc(opt, i);
+        }
     }
     if (neighbour == 'c'){
-        cFunc(opt[i + 1], opt, i+1);
+        if (isMassiveFinished(opt, i) != 1){
+            cFunc(opt[i], opt, i);
+        }
     }
 }
 
 int bFunc(char* opt[], int* index){
+    printf ("b ");
+    int i = index + 1;
+    free(index);
 
+    if (isMassiveFinished(opt, i) == 1){
+        printf("[ERROR] The -b option has no argument");
+        return 1;
+    }
+    
+    int start = index;
+    int end = isMassiveFinished(opt, i) - 1;
 }
 
 int cFunc(char* neighbour, char* opt[], int* index){
+    printf("c\n");
     int i = index + 1;
     free(index);
     if (neighbour == 'a'){
-        aFunc(opt[i], opt, i);
+        if (isMassiveFinished(opt, i) != 1){
+            aFunc(opt[i], opt, i);
+        }
     }
     if (neighbour == 'b'){
-        bFunc(opt, i);
+        if (isMassiveFinished(opt, i) != 1){
+            bFunc(opt, i);
+        }
     }
     if (neighbour == 'c'){
-        cFunc(opt[i], opt, i);
+        if (isMassiveFinished(opt, i) != 1){
+            cFunc(opt[i], opt, i);
+        }
     }
+}
+
+int isMassiveFinished(char* opt, int* index){
+    size_t sizeOpt = sizeof(opt);
+    if (index >= sizeOpt){
+        return 1;
+    }
+    return sizeOpt;
 }
