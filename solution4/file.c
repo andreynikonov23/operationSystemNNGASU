7e66b6d46237feb9f74731662439815ad77dfc1c
andrey <xxxkeep3rxxx@gmail.com>
2023-11-09 16:24:37 +0300

    09.11

--- solution4/file.c
+++ solution4/file.c
@@ -1,56 +1,51 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <unistd.h>
-#include <wait.h>
 #include <string.h>
 
+int aFunc(char neighbour, char* opt, int index);
+int bFunc(char* opt, int index);
+int cFunc(char neighbour, char* opt, int index);
+int isMassiveFinished(char * opt, int index);
 
-int main(int argc, char const *argv[]){
-    char error[] = "[ERROR] Not valid options";
 
-    for (int i=1; i<argc; i++){
-        //Option
-        char opt[150];
-        strcpy(opt, argv[i]);
-        
-        //Basic case
-        if (opt[0] != '-'){
-            printf("%s", error);
+int main(int argc, char* argv[]){
+    char error[] = "[ERROR] Not valid argument";
+
+    for(int i=1; i<argc; i++){
+        char* opt = argv[i];
+
+        if (opt[0] -= '-'){
+            printf("%s\n", error);
         }
-
         int index = 1;
-        char o = opt[index];
-        if (o == 'a'){
-            if (isMassiveFinished(opt, index+1) != 1){
-                aFunc(opt[index + 1], opt, index + 1);
-            } else {
-                printf("a\n");
+        char c = opt[index];
+
+        if(c == 'a'){
+            printf("a\n");
+            if (isMassiveFinished(opt, index + 1) != 1){
+                aFunc(opt[index + 1], opt, index +1);
             }
         }
-        if (o == 'b'){
-            if (isMassiveFinished(opt, index+1) != 1){
-                bFunc(opt, index + 1);
-            } else {
-                printf("b\n");
-            }
+        if(c == 'b'){
+            printf("b\n");
+            bFunc(opt, index + 1);
         }
-        if (o == 'c'){
-            if (isMassiveFinished(opt, index+1) != 1){
-                cFunc(opt[index + 1], opt, index + 1);
-            } else {
-                printf("c\n");
+        if(c == 'c'){
+            printf("c\n");
+            if (isMassiveFinished(opt, index + 1) != 1){
+                cFunc(opt[index + 1], opt, index +1);
             }
         }
     }
-
     return 0;
 }
 
-int aFunc(char neighbour, char* opt[], int index){
+int aFunc(char neighbour, char* opt, int index){
     printf("a\n");
+
     if (neighbour == 'a'){
         if (isMassiveFinished(opt, index + 1) != 1){
-            aFunc(opt[index], opt, index + 1);
+            aFunc(opt[index + 1], opt, index);
         }
     }
     if (neighbour == 'b'){
@@ -66,27 +61,7 @@ int aFunc(char neighbour, char* opt[], int index){
     return 0;
 }
 
-int bFunc(char* opt[], int index){
-    printf ("b ");
-
-    if (isMassiveFinished(opt, index + 1) == 1){
-        printf("[ERROR] The -b option has no argument");
-        return 1;
-    }
-    
-    int start = index;
-    size_t sizeOpt = sizeof(opt);
-    int size = sizeOpt - start;
-    char arg[size];
-    for(int i=0; i<size; i++){
-        arg[i] = opt[start];
-        start++;
-    }
-    printf("with argument %s\n", arg);
-    return 0;
-}
-
-int cFunc(char neighbour, char* opt[], int index){
+int cFunc(char neighbour, char* opt, int index){
     printf("c\n");
     
     if (neighbour == 'a'){
@@ -107,14 +82,31 @@ int cFunc(char neighbour, char* opt[], int index){
     return 0;
 }
 
-int isMassiveFinished(char * opt[], int index){
-    size_t sizeOpt = sizeof(opt);
-    int size = 0;
-    for (int i=0; i<sizeOpt; i++){
-        size++;
-    }
-    if (index >= size){
+int bFunc(char* opt, int index){
+    printf ("b ");
+
+    if (isMassiveFinished(opt, index + 1) == 1){
+        printf("[ERROR] The -b option has no argument");
         return 1;
     }
-    return size;
+    
+    int start = index;
+    size_t sizeOpt = sizeof(opt);
+    int size = sizeOpt - start;
+    char arg[size];
+    for(int i=0; i<size; i++){
+        arg[i] = opt[start];
+        start++;
+    }
+    printf("with argument %s\n", arg);
+    return 0;
+}
+
+
+int isMassiveFinished(char * opt, int index){
+    int length = strlen(opt);
+    if (index >= length){
+        return 1;
+    }
+    return length;
 }
